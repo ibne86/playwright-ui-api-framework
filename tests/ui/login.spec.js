e064440014323a8c5c871daf402d11ca47e6b2ec
Ibne Sina Khan <khanibne@outlook.com>
2026-01-27 20:01:06 +0100

    refactor(ui): read UI test data from test-data folder

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -1,31 +1,35 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/LoginPage.js";
+import { users, uiText, errors } from "../../test-data/ui/index.js";
 
 test("UI: Valid user can login", async ({ page }) => {
   const login = new LoginPage(page);
+
   await login.goto();
-  await login.loginSuccess("standard_user", "secret_sauce");
-  await expect(page.locator(".title")).toHaveText("Products");
+  await login.loginSuccess(users.standard.username, users.standard.password);
+  await expect(page.locator(".title")).toHaveText(uiText.productsTitle);
 });
 
 test("UI: Locked out user cannot login", async ({ page }) => {
   const login = new LoginPage(page);
-    await login.goto();
-    await login.invalidLogin("locked_out_user", "secret_sauce");
-    await expect(login.error).toContainText("Epic sadface: Sorry, this user has been locked out."
-);
+
+  await login.goto();
+  await login.invalidLogin(users.lockedOut.username, users.lockedOut.password);
+  await expect(login.error).toContainText(errors.lockedOut);
 });
 
 test("UI: Invalid user cannot login", async ({ page }) => {
   const login = new LoginPage(page);
-    await login.goto();
-    await login.invalidLogin("invalid_user", "invalid_pass"); 
-    await expect(login.error).toContainText("Epic sadface: Username and password do not match any user in this service");
+
+  await login.goto();
+  await login.invalidLogin(users.invalid.username, users.invalid.password);
+  await expect(login.error).toContainText(errors.invalidUser);
 });
 
 test("UI: Cannot login by empty credentials", async ({ page }) => {
   const login = new LoginPage(page);
-    await login.goto();
-    await login.invalidLogin("", ""); 
-    await expect(login.error).toContainText("Epic sadface: Username is required");
+  
+  await login.goto();
+  await login.invalidLogin(users.empty.username, users.empty.password);
+  await expect(login.error).toContainText(errors.usernameRequired);
 });
--- tests/ui/logout.spec.js
+++ tests/ui/logout.spec.js
@@ -1,15 +1,14 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/LoginPage.js";
 import { ProductsPage } from "../../pages/ProductsPage.js";
+import { users } from "../../test-data/ui/index.js";
 
 test("UI: user can logout and return to login page", async ({ page }) => {
   const login = new LoginPage(page);
   const products = new ProductsPage(page);
 
   await login.goto();
-  await login.loginSuccess("standard_user", "secret_sauce");
-
+  await login.loginSuccess(users.standard.username, users.standard.password);
   await products.logout();
-
   await expect(page.getByTestId("login-button")).toBeVisible();
 });
