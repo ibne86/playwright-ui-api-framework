000a5b7d148d0425b2e1654c7025658fea449521
Ibne Sina Khan <khanibne@outlook.com>
2026-01-27 20:00:41 +0100

    test(data): add shared UI test data modules

---
+++ test-data/ui/users.js
@@ -0,0 +1,6 @@
+export const users = {
+  standard: { username: "standard_user", password: "secret_sauce" },
+  lockedOut: { username: "locked_out_user", password: "secret_sauce" },
+  invalid: { username: "invalid_user", password: "invalid_pass" },
+  empty: { username: "", password: "" },
+};
